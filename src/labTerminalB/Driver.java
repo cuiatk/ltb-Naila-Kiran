77e897bdfd5fc1cc40801f4bf78c839440147f83
Naila kiran <nailakiran038@gmail.com>
2019-06-13 10:43:56 +0500

    TODO 1 completed Regular expresssion

--- src/labTerminalB/Driver.java
+++ src/labTerminalB/Driver.java
@@ -10,9 +10,10 @@ public class Driver {
 	 * 		"TODO 'TASK NUMBER' Complete" e.g. in this case "TODO 1 COMPLETED"
 	 */
 	public static void main(String[] args) {
+		
 		String input = "The <b>Good</b>, the <i>Bad</i>, and the <strong>Ugly</strong>";
 		String regex = "TODO 1";
-		String output = input.replaceAll(regex, "");
+		String output = input.replaceAll("<[^>]*>", "");
 		System.out.println(output);
 		
 		
